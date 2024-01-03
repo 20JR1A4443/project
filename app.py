c9bfb675785b17d8d299fee3db928ae7da2ab6a3
MohanRaviTeja <mohanravitejakakumanu@gmail.com>
2024-01-03 08:43:12 +0530

    one

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+@app.route("/secound")
+def secound():
+    return 'i love'
+
+@app.route('/<name>/')
+def Ilove(name):
+    return f'I love you {name}'
+# if __name__ == '__main__':
+#     app.run(use_reloader=True, debug=True)
