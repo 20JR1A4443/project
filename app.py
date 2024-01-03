c8a5f69660f61e4ed91263db641e4dd193095d11
baramsivaramireddy <baramsivarami@gmail.com>
2024-01-03 10:41:56 +0530

    add pickle file

--- app.py
+++ app.py
@@ -3,11 +3,25 @@ from flask import render_template
 from flask import request
 from args import *
 app = Flask(__name__)
-print(location_mapping)
+import numpy as np
+import pickle
+with open('Model.pkl', 'rb') as mod:
+    model = pickle.load(mod)
+with open('Scaler.pkl' ,'rb') as mod:
+    scaler = pickle.load(mod)
 @app.route("/" , methods=['GET' ,'POST'])
 def index():
     if request.method == 'POST':
-        return request.form  
+        bedrooms = request.form['bedrooms']
+        bathrooms = request.form['bathrooms']
+        location = request.form['location']
+        sqf = request.form['sqf']
+        status = request.form['status']
+        direction = request.form['direction']
+        property_type = request.form['property type']
+        input_array = np.array([[
+            bedrooms,bathrooms,location,sqf,status,direction,property_type
+        ]])
     else:
         return render_template('index.html',location_mapping=location_mapping,
                                status_mapping=status_mapping,
