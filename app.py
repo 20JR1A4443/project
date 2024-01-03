955771f2a1066741ed55cab5f5c2cf0ceb038b06
baramsivaramireddy <baramsivarami@gmail.com>
2024-01-03 10:11:21 +0530

    added selection

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request
+from args import *
 app = Flask(__name__)
-
+print(location_mapping)
 @app.route("/" , methods=['GET' ,'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return request.form  
+    else:
+        return render_template('index.html',location_mapping=location_mapping,
+                               status_mapping=status_mapping,
+                               direction_mapping=direction_mapping,
+                               property_type_mapping=property_type_mapping)
 @app.route("/secound")
 def secound():
     return 'i love'
